50f30570b2b12ba30d7e09536cfc59ab9b83ed45
Valelearn1 <valentina-ferro@hotmail.it>
2026-06-03 16:50:36 +0200

    blocco scroll body e chiusura con escape

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,26 @@ const chiudiContatti = document.getElementById("chiudi-contatti"); // Cerco il p
 apriContatti.addEventListener("click", () => {
   // Quando clicco sul pulsante Apri contatti,
   overlay.removeAttribute("hidden"); // tolgo l'attributo hidden, quindi l'overlay diventa visibile
+  document.body.style.overflow = "hidden";
 });
 
 // Chiusura con il pulsante X
 chiudiContatti.addEventListener("click", () => {
   // Quando clicco sul pulsante Chiudi contatti,
   overlay.setAttribute("hidden", ""); // aggiungo hidden, quindi l'overlay torna nascosto
+  document.body.style.overflow = "";
 });
 
 // Chiusura cliccando sullo sfondo
 overlay.addEventListener("click", (event) => {
   if (event.target === overlay) {
+    overlay.setAttribute("hidden", "");
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    console.log(overlay);
     overlay.setAttribute("hidden", "");
   }
 });
